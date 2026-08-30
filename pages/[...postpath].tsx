a807c6275a8c6026eb41cad0c1e33f6caa893539
full-vidios-online <parvessikdar101010@gmail.com>
2026-08-30 15:12:56 +0600

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://movie-streama-online.blogspot.com/p/v1.html`
+					`https://us-news-hub.blogspot.com`
 				}`,
 			},
 		};
