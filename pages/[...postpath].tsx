6f9361106aa9457be10e366ec72487beb93625c0
full-vidios-online <parvessikdar101010@gmail.com>
2026-09-09 17:15:59 +0600

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://www.linkss45xc.dpdns.org/p/v1.html?utm_source=Facebook&utm_medium=Social&utm_campaign=paru`
+					`https://vip.mixclips.top/mix44vip/?utm_source=Saim&utm_medium=Saim100`
 				}`,
 			},
 		};
